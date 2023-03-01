c1cba7b5ce4bf83851159351738087a5ba276e3c
pazdiaz <paz.diaz.osorio@gmail.com>
2023-02-28 22:24:53 -0300

    probandoPat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
+document.getElementById("btn").addEventListener("click",function(e){ //funcion boton validar//
+    e.preventDefault()
+    const inpunt =document.getElementById ("inputNumero")
+    const result = document.getElementById("result");
+    const valid = validator.isValid(inpunt.value)
+    const mascara = validator.maskify (inpunt.value)
+    console.log (mascara); 
+    if (valid) {
+        result.innerHTML = "Su tarjeta " + mascara + " es Valida"  //resultado html
+        //exitoso
+   inpunt.value = "";
+    }
+    else {
+        result.innerHTML = "Su tarjeta " + mascara +" es invalida, vuelva a intentarlo" //resultado html
+        //error
+        inpunt.value = "";
+    }
+})
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,72 @@
 const validator = {
-  // ...
-};
+  isValid(creditCardNumber) {
+    let process = creditCardNumber.trim() //">numero" //.trim elimina los espacios en blanco de ambos extremos de una cadena
+    console.log(process)
+    if (process.length === 0) {
+      return false
+    }
+    let arrayNUmeroTarjeta = process.split("") //dividir array
 
+    let nuevoArrayInvertido = arrayNUmeroTarjeta.reverse(); // invertir
+
+    let arrayPar = nuevoArrayInvertido.filter(function (x, y) {  //numeros pares
+      if (y % 2 === 1) return x;
+    });
+
+    let arrayImpar = nuevoArrayInvertido.filter(function (x, y) { //numeros impares
+      if (y % 2 === 0) return x;
+    });
+    console.log("arrayPar", arrayPar)
+    console.log("arrayImpar", arrayImpar)
+
+    let arrayplus = arrayPar.map(function (parmulti) {
+      let multiplicar = parmulti * 2;
+      console.log(multiplicar);
+
+      if (multiplicar >= 10) {
+        let diezmayor = multiplicar.toString().split("").map(Number); //se convierte en string para split
+
+        let arraytres = diezmayor.reduce(function (x, z) {  //.reduce sumamos 
+          return x + z;
+        });
+
+        return arraytres; //ojo (suma)
+      }
+      else { return multiplicar } //ojo
+    });
+    arrayImpar = arrayImpar.map(Number)
+
+    let sumaplus = arrayplus.reduce(function (x, y) {
+      return x + y //(Number o parseInt())
+    }) //cosolar 
+    let sumaImpar = arrayImpar.reduce(function (x, z) {
+      return x + z;
+    })//cosolar
+    let resultado = sumaplus + sumaImpar;
+    //cosolar 
+    if (resultado % 10 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+  },
+  //funcion de maskify
+  maskify(numero) {
+    numero = numero.toString()
+   let acumulador = ""
+    for (let i = 0; i < numero.length; i++) {
+      if (i < numero.length - 4) {
+        acumulador = acumulador + numero [i]
+      } else {
+        acumulador = acumulador + "#"
+      }
+    }
+    return acumulador
+  }
+};
+     
+     
 export default validator;
+
