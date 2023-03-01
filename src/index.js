85162bacfe384e1e5dac4b04edbd3f5443d16fdf
pazdiaz <paz.diaz.osorio@gmail.com>
2023-03-01 14:33:06 -0300

    primercommit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import validator from './validator.js';
-document.getElementById("btn").addEventListener("click",function(e){ //funcion boton validar//
+document.getElementById("btn").addEventListener("click", function (e) { //funcion boton validar//
     e.preventDefault()
-    const inpunt =document.getElementById ("inputNumero")
+    const inpunt = document.getElementById("inputNumero") //input es getElementById
     const result = document.getElementById("result");
-    const valid = validator.isValid(inpunt.value)
-    const mascara = validator.maskify (inpunt.value)
-    console.log (mascara); 
+    const valid = validator.isValid(inpunt.value) 
+    const mascara = validator.maskify(inpunt.value)
+    console.log(mascara);
     if (valid) {
         result.innerHTML = "Su tarjeta " + mascara + " es Valida"  //resultado html
         //exitoso
-   inpunt.value = "";
+        inpunt.value = "";
     }
     else {
-        result.innerHTML = "Su tarjeta " + mascara +" es invalida, vuelva a intentarlo" //resultado html
+        result.innerHTML = "Su tarjeta " + mascara + " es invalida, vuelva a intentarlo" //resultado html
         //error
         inpunt.value = "";
     }
